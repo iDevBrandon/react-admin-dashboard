ac52bac60a6b70a3284599306029c8e415d5fca5
Brandon Ha <idevbrandon@gmail.com>
2022-02-21 16:48:39 +0900

    [feat] styling watchlist widget

--- client/src/components/Widgets/WatchlistWidget/WatchlistWidget.js
+++ client/src/components/Widgets/WatchlistWidget/WatchlistWidget.js
@@ -1,8 +1,90 @@
 import React from "react";
-import { WatchlistContainer } from "./styles";
+import {
+  WatchList,
+  WatchListButton,
+  WatchlistContainer,
+  WatchListItem,
+  WatchListItemImg,
+  WatchListItemInfo,
+  WatchListItemSector,
+  WatchListItemTitle,
+  WatchlistTitle,
+} from "./styles";
+import { AiFillEye } from "react-icons/ai";
 
 const WatchlistWidget = () => {
-  return <WatchlistContainer>Watchlist</WatchlistContainer>;
+  return (
+    <WatchlistContainer>
+      <WatchlistTitle>Watchlist</WatchlistTitle>
+      <WatchList>
+        <WatchListItem>
+          <WatchListItemImg
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Apple-logo.png/640px-Apple-logo.png"
+            alt="Apple logo"
+          />
+          <WatchListItemInfo>
+            <WatchListItemTitle>Apple</WatchListItemTitle>
+            <WatchListItemSector>Technology</WatchListItemSector>
+          </WatchListItemInfo>
+          <WatchListButton>
+            <AiFillEye /> Detail
+          </WatchListButton>
+        </WatchListItem>
+        <WatchListItem>
+          <WatchListItemImg
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Apple-logo.png/640px-Apple-logo.png"
+            alt="Apple logo"
+          />
+          <WatchListItemInfo>
+            <WatchListItemTitle>Apple</WatchListItemTitle>
+            <WatchListItemSector>Technology</WatchListItemSector>
+          </WatchListItemInfo>
+          <WatchListButton>
+            <AiFillEye /> Detail
+          </WatchListButton>
+        </WatchListItem>
+        <WatchListItem>
+          <WatchListItemImg
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Apple-logo.png/640px-Apple-logo.png"
+            alt="Apple logo"
+          />
+          <WatchListItemInfo>
+            <WatchListItemTitle>Apple</WatchListItemTitle>
+            <WatchListItemSector>Technology</WatchListItemSector>
+          </WatchListItemInfo>
+          <WatchListButton>
+            <AiFillEye /> Detail
+          </WatchListButton>
+        </WatchListItem>
+        <WatchListItem>
+          <WatchListItemImg
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Apple-logo.png/640px-Apple-logo.png"
+            alt="Apple logo"
+          />
+          <WatchListItemInfo>
+            <WatchListItemTitle>Apple</WatchListItemTitle>
+            <WatchListItemSector>Technology</WatchListItemSector>
+          </WatchListItemInfo>
+          <WatchListButton>
+            <AiFillEye /> Detail
+          </WatchListButton>
+        </WatchListItem>
+        <WatchListItem>
+          <WatchListItemImg
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Apple-logo.png/640px-Apple-logo.png"
+            alt="Apple logo"
+          />
+          <WatchListItemInfo>
+            <WatchListItemTitle>Apple</WatchListItemTitle>
+            <WatchListItemSector>Technology</WatchListItemSector>
+          </WatchListItemInfo>
+          <WatchListButton>
+            <AiFillEye /> Detail
+          </WatchListButton>
+        </WatchListItem>
+      </WatchList>
+    </WatchlistContainer>
+  );
 };
 
 export default WatchlistWidget;
--- client/src/components/Widgets/WatchlistWidget/styles.js
+++ client/src/components/Widgets/WatchlistWidget/styles.js
@@ -12,3 +12,51 @@ export const WatchlistContainer = styled.div`
     margin-right: 0.5rem;
   }
 `;
+
+export const WatchListItemImg = styled.img`
+  height: 40px;
+  object-fit: cover;
+`;
+
+export const WatchlistTitle = styled.span`
+  font-size: 1.3rem;
+  font-weight: 600;
+`;
+
+export const WatchList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+export const WatchListItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1rem 0;
+`;
+
+export const WatchListItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const WatchListItemTitle = styled.span`
+  font-weight: 600;
+`;
+
+export const WatchListItemSector = styled.span`
+  font-weight: 200;
+  color: grey;
+`;
+
+export const WatchListButton = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  border-radius: 0.8rem;
+  cursor: pointer;
+  padding: 0.5rem;
+  background-color: #eeeef7;
+  color: #555;
+`;

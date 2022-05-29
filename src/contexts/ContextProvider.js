fdc42e45758963d29e0f56a78a2edd70701a8da5
Brandon Ha <idevbrandon@gmail.com>
2022-05-29 17:54:27 +0900

    feat : handle click for nav components

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,7 +11,12 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
-  return <StateContext.Provider value={{ activeMenu, setActiveMenu }}>{children}</StateContext.Provider>;
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+  return <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick }}>{children}</StateContext.Provider>;
 };
 
 export const useStateContext = () => useContext(StateContext);
